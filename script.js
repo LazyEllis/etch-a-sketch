814a8efb1b653f335098358bd36014c66a8ff847
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2023-08-07 00:30:09 +0100

    Add color effect to squares on hover

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ for (let i = 1; i <= 16; i++) {
 for (let j = 1; j <= 16; j++) {
     gridContainer.appendChild(gridRow.cloneNode(true));
 }
+
+const gridSquares = document.querySelectorAll(".grid-square");
+
+gridSquares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.classList.add("colored");
+    });
+});
